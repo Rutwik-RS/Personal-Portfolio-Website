04fceb7ff986dd9f4cf996114b1f7b5bab4062b2
Rutwik-RS <23311a0594@cse.sreenidhi.edu.in>
2025-03-02 15:55:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("nav ul li a").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            const target = document.querySelector(this.getAttribute("href"));
+            target.scrollIntoView({ behavior: "smooth" });
+        });
+    });
+});
